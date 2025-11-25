a250d6a0d12bb7d5242e9bb101c3bc20168abcad
Diyorbek <dedamirzayevdiyorbek9@gmail.com>
2025-11-25 12:08:59 +0500

    Second commit

--- problem1.py
+++ problem1.py
@@ -1,11 +1,28 @@
 def are_anagrams(string1, string2):
-    string1= string1.strip().lower()
-    string2=string2.strip().lower()
-    for c in string1:
-        if not c in string2:
-            return False
-        else:
-            return True
+    string1=string1.strip().lower().replace(' ', '').replace('.', '')
+    string2=string2.strip().lower().replace(' ', '').replace('.', '')
+    new_list1=list(string1)
+    new_list2=list(string2)
+    new_list1.sort()
+    new_list2.sort()
+    s1="".join(new_list1)
+    s2="".join(new_list2)
+    print(s1)
+    print(s2)
+    if s1==s2:
+        return True
+    else:
+        return False
+
+
+
+
+    # len()
+    # for c in string1:
+    #     if not c in string2:
+    #         return False
+    #     else:
+    #         return True
         
 print(are_anagrams("Listen", "Silent"))
 print(are_anagrams("The Morse Code", "Here come dots"))
